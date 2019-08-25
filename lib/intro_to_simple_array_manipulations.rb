71feb0e6e1896ff3526658975bed45d2482b24a5
Warren McDonald <warrenjmac@mail.com>
2019-08-25 08:17:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -33,8 +33,8 @@ def shift_with_args(arguments)
 end
 
 def using_concat(array1, array2)
-  @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-  more_favs = ["sports cars", "flatiron school"]
+  #@my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+  #more_favs = ["sports cars", "flatiron school"]
   @all_my_favs = @my_favorite_things.concat(more_favs)
 end
 
